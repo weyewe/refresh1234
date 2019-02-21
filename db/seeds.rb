0db5761d4f65516d33166fbebf341a7e038bd0dd
weyewe1 <rajakuraemas@gmail.com>
2019-02-21 07:11:59 +0000

    add new

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+ 
+ 
+#  to start 
+ 
+#      /usr/lib/postgresql/9.5/bin/pg_ctl -D /var/lib/postgresql/9.5/main -l logfile start
+
+
+# /etc/postgresql/9.5/main/pg_hba.conf
+# https://community.c9.io/t/can-we-upgrade-to-postgres-9-4/3897/4
